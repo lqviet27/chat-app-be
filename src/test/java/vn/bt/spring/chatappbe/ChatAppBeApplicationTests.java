148e3a1f78c92344a9476523720a3ed5358b10cf
Le Quoc Viet <lequocviet2752004@gmail.com>
2024-08-18 15:26:28 +0700

    init

---
+++ src/test/java/vn/bt/spring/chatappbe/ChatAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.bt.spring.chatappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
